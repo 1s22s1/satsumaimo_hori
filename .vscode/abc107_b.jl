538312212cad9fa4afaa71a225cd5c69b6062b1c
1s22s1 <1s22s1@users.noreply.github.com>
2024-10-09 18:56:17 +0900

    Solve

---
+++ .vscode/abc107_b.jl
@@ -0,0 +1,20 @@
+function parseint()
+    return readline() |> x -> parse(Int, x)
+end
+function parsestring()
+    return readline()
+end
+
+function parseints()
+    return readline() |> split |> x -> parse.(Int, x)
+end
+
+function parsestrings()
+    return readline() |> split
+end
+
+function solve()
+    gomora
+end
+
+solve()

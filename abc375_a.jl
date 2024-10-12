5b2ac589233d66ce3fe1ddbe43b281f6a32c98c8
1s22s1 <1s22s1@users.noreply.github.com>
2024-10-12 21:59:49 +0900

    Solve

--- abc375_a.jl
+++ abc375_a.jl
@@ -7,7 +7,18 @@ parseints() = readline() |> split |> x -> parse.(Int, x)
 parsestrings() = readline() |> split
 
 function solve()
-    gomora
+    n = parseint()
+    s = split(parsestring(), "")
+
+    outcome = 0
+
+    for i ∈ 1:n-2
+        if s[i] == "#" && s[i+1] == "." && s[i+2] == "#"
+            outcome += 1
+        end
+    end
+
+    println(outcome)
 end
 
 solve()
--- abc375_b.jl
+++ abc375_b.jl
@@ -6,8 +6,24 @@ parseints() = readline() |> split |> x -> parse.(Int, x)
 
 parsestrings() = readline() |> split
 
+using Printf
+
 function solve()
-    gomora
+
+    n = parseint()
+    positions = [parseints() for _ ∈ 1:n]
+
+    outcome = 0
+
+    pushfirst!(positions, [0, 0])
+    push!(positions, [0, 0])
+
+    for i ∈ 1:length(positions)-1
+        distance = positions[i] - positions[i+1] |> x -> x .^ 2 |> sum |> x -> sqrt(x)
+        outcome += distance
+    end
+
+    @printf("%.10f\n", outcome)
 end
 
 solve()
